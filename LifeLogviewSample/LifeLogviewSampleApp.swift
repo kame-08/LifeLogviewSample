5bf4485a2da2b0826837ea6e7e82a6da9ea3d816
Kame2021 <21cm0108@jec.ac.jp>
2022-10-07 10:45:47 +0900

    Initial Commit

---
+++ LifeLogviewSample/LifeLogviewSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  LifeLogviewSampleApp.swift
+//  LifeLogviewSample
+//
+//  Created by Ryo on 2022/10/07.
+//
+
+import SwiftUI
+
+@main
+struct LifeLogviewSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
